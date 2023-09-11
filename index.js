31864f6c439084e2ba4294352a350dff5173b509
Hannahjef <105321681+Hannahjef@users.noreply.github.com>
2023-09-10 21:59:11 -0700

    UI change and algorithm change

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
 let playerNameArray = [];
+let teamBlue=[]
+let teamGreen = []
+let teamRed = []
+let generateId = document.getElementById("counter-num")
+let generateTeam = document.getElementById("counter-team")
+let playerCount =2
+let teamCount=2
+let rows =[];
+const plCount = document.querySelector('#playerCount');
+const playList = document.querySelector("#newtable")
+const new_Table = document.querySelector("#teamtabletable")
+
+
 window.addEventListener('load',() => {
     const form = document.querySelector('#addPlayerForm');
     const input = document.querySelector('#addplayername');
     const list_el = document.querySelector('#playerlist');
-
+    
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         
@@ -50,7 +63,7 @@ window.addEventListener('load',() => {
 
             list_el.appendChild(task_el);
             input.value='';
-
+            plCount.innerText= "Current Players Count : " +playerNameArray.length
 
 
             task_delete_el.addEventListener('click', (e) => {
@@ -63,30 +76,14 @@ window.addEventListener('load',() => {
                         playerNameArray.splice(i,1);
                     }
                 }
+                plCount.innerText= "Current Players Count : " +playerNameArray.length
             });
 
     })
 })
-let generateId = document.getElementById("counter-num")
-let generateTeam = document.getElementById("counter-team")
-let playerCount =2
-let teamCount=1
 
-function incrementPlayers(){
-if(playerCount<12)
-{
-    playerCount+=1
-generateId.innerText=playerCount
-}
 
-}
-function decrementPlayers(){
-    if(playerCount>2)
-    {
-        playerCount-=1
-    generateId.innerText=playerCount
-    }
-}
+
     function decrementTeam(){
        
         if(teamCount>1)
@@ -111,22 +108,36 @@ function decrementPlayers(){
         }
 
         function assignTeams(){
+            teamBlue=[];
+            teamGreen=[];
+            teamRed=[];
+            for(let i=0;i<playerNameArray.length;i++)
+            {
+                let temp = playerNameArray[i];
+                let rand = randomNumber(0,playerNameArray.length);
+                playerNameArray[i] = playerNameArray[rand];
+                playerNameArray[rand]=temp;
+            }
             
+            for( let i=0;i<playerNameArray.length;i++)
+            {
+                console.log(playerNameArray[i])
+            }
             if(teamCount<2 || playerNameArray.length%teamCount!=0 )
             {
                 if(teamCount<2)
                 {
-                    console.log("Invalid team count");
+                    alert("Invalid team count");
+                    return;
                 }
                 else
-                console.log("Invalid player count");
+               alert("Invalid no of Players");
+               return;
             }
             else if(teamCount==2){
                 
                 let playerPerTeam = playerNameArray.length/teamCount;
                 console.log(playerPerTeam)
-                let teamBlue=[]
-                let teamGreen = []
                 for (let i=0;i<playerNameArray.length;i++)
                 {
                     let randomValue = Math.random(1,500023);
@@ -145,19 +156,162 @@ function decrementPlayers(){
                         teamBlue.push(playerNameArray[i])
                     }
                 }
-                console.log("team 1")
-                for(let i=0;i<teamBlue.length;i++)
-                {
-                    console.log(teamBlue[i]);
-                }
-                console.log("team 2")
-                for(let i=0;i<teamGreen.length;i++)
-                {
-                    console.log(teamGreen[i]);
-                }
                 
 
-
             }
+            else if(teamCount==3)
+            {
+                for(let player=0;player<playerNameArray.length;player++)
+                {
+    
+                    
+                    let playerPerTeam = playerNameArray.length/teamCount;
+                    let status = false;
+                   
+                    while(status==false)
+                    {
+                        let team;
+                        let randomteam = randomNumber(0,3);
+
+                        
+                    if(randomteam==0)
+                    {
+                       team = teamBlue;
+                       
+                    }
+                    else if(randomteam==1)
+                    {
+                        team = teamGreen;
+
+                    }
+                    else{
+                        team = teamRed;
+                    }
+
+                    if(team.length==playerPerTeam)
+                    {
+                        continue;
+                    }
+                    else{
+                        team.push(playerNameArray[player]);
+                        status = true;
+                    }
+                    
+                }
+
+                    
+                }
+            }
+            console.log("Team1")
+            for(let i=0;i<teamBlue.length;i++)
+            {
+                console.log(teamBlue[i])
+            }
+            console.log("Team Green")
+            for(let i=0;i<teamGreen.length;i++)
+            {
+                console.log(teamGreen[i])
+            }
+            console.log("Team Red")
+            for(let i=0;i<teamRed.length;i++)
+            {
+                console.log(teamRed[i])
+            }
+            generateTable() 
+        }
+
+
+        function generateTable() {
+            if(rows.length!=0)
+            {
+                new_Table.removeChild(rows[0]);
+                rows.pop();
+            }
+            //new_List.removeChild();
+            //playList.removeChild();
+            const new_List = document.createElement("div");
+            // creates a <table> element and a <tbody> element
+            
+            const myBody = document.getElementsByTagName("body")[0];
+
+            // creates <table> and <tbody> elements
+          
+            const myTableBody = document.createElement("tbody");
+            
+
+            // creating all cells
+            for (let j = 0; j <= teamBlue.length; j++) {
+              // creates a <tr> element
+              const myCurrentRow = document.createElement("tr");
+    
+              for (let i = 0; i < teamCount; i++) {
+                // creates a <td> element
+                const myCurrentCell = document.createElement("td");
+                let textvalue ="";
+                // creates a Text Node
+                if(j==0)
+                {
+                    myCurrentCell.style.color="white";
+                switch(i)
+                {
+                    
+                    case 0:
+                        textvalue = 'Team Blue';
+                        myCurrentCell.style.backgroundColor="blue";
+                        break;
+                    case 1:
+                        textvalue = 'Team Green';
+                        myCurrentCell.style.backgroundColor="green";
+                        break;
+                    case 2:
+                        textvalue = 'Team Red';
+                        myCurrentCell.style.backgroundColor="red";
+                        break;
+                }   
+            }
+            else
+            {
+                switch(i)
+                {
+                    case 0:
+                        textvalue = teamBlue[j-1];
+                        console.log(teamBlue[j]-1)
+                        break;
+                    case 1:
+                        textvalue = teamGreen[j-1];
+                        break;
+                    case 2:
+                        textvalue = teamRed[j-1];
+                        break;
+                }   
+            }
+            const currentText = document.createTextNode(
+                textvalue,
+              );
+                // appends the Text Node we created into the cell <td>
+                myCurrentCell.appendChild(currentText);
+                // appends the cell <td> into the row <tr>
+                myCurrentRow.appendChild(myCurrentCell);
+              }
+              // appends the row <tr> into <tbody>
+              myTableBody.appendChild(myCurrentRow);
+              
+            }
+    
+            // appends <tbody> into <table>
+            new_Table.appendChild(myTableBody);
+            new_List.appendChild(new_Table);
+            playList.appendChild(new_List);
+            rows.push(myTableBody);
+            // appends <table> into <body>
+            // myBody.appendChild(myTable);
+            // sets the border attribute of myTable to 2;
+            //myTable.setAttribute("border", "1");
+          
+            
+          }
+
+          function randomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min) + min);
         }
     
